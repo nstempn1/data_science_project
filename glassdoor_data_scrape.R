9954fd627fc88b9cdbda61680b3b48ac362f79a8
Nikita Stempniewicz <nstempni@gmail.com>
2017-09-18 12:33:13 -0400

    added comments in code

--- glassdoor_data_scrape.R
+++ glassdoor_data_scrape.R
@@ -5,27 +5,37 @@ library(XML)
 # Store web url
 url <- read_html("https://www.glassdoor.com/Job/data-scientist-jobs-SRCH_KO0,14.htm")
 
+#get location of position
 location<-url %>%  html_nodes(".empLoc .loc") %>% html_text()
 location
 
+#get employer of position
 employer<-url %>%  html_nodes("i")%>% html_attr("data-employer-shortname")
 employer <- employer[!is.na(employer)]
 
+#get employer id of position
+#I want to use this to get more data on employers
 employer_id<-url %>%  html_nodes("i")%>% html_attr("data-employer-id")
 employer_id <- employer_id[!is.na(employer_id)]
 
+#get id of position
+#I want to try and use this to ge more information about job posts
 job_id<-url %>%  html_nodes("i")%>% html_attr("data-job-id")
 job_id <- job_id[!is.na(job_id)]
 
+#get title of position
 title<-url %>%  html_nodes("i")%>% html_attr("data-jobtitle")
 title <- title[!is.na(title)]
 
+#get minimum salary estimate of position
 min_salary<-url %>%  html_nodes("i")%>% html_attr("data-displayed-min-salary")
 min_salary <- min_salary[!is.na(min_salary)]
 
+#get maximum salary estimate of position
 max_salary<-url %>%  html_nodes("i")%>% html_attr("data-displayed-max-salary")
 max_salary <- max_salary[!is.na(max_salary)]
 
+#get median salary estimate of position
 med_salary<-url %>%  html_nodes("i")%>% html_attr("data-displayed-med-salary")
 med_salary <- med_salary[!is.na(med_salary)]
 
@@ -38,7 +48,9 @@ glassdoor_b<-url %>%  html_nodes(xpath = '//*[@class="flexbox empLoc"]')%>%  htm
 
 glassdoor_b
 
-# Store web url
+
+#the code above only gets information for the first page of results
+#the code below is a loop for the remaining pages
 
 for (i in 2:907){
  
@@ -79,4 +91,6 @@ glassdoor_b<- c(glassdoor_b, partb)
 
 glassdoor_a
 glassdoor_b
+
+
 
