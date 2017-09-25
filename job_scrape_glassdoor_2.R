e2ae767e052f407b82a0519676d970c9bc29521b
Nikita Stempniewicz <nstempni@gmail.com>
2017-09-25 14:18:36 -0400

    Final dataset with fixed code, also added some text to the RMD around the methods used to get the data

--- job_scrape_glassdoor_2.R
+++ job_scrape_glassdoor_2.R
@@ -4,13 +4,13 @@
 ###To-do
 #figure out how to cite code
 #figure out how to package libraries
+#add code to last part that keeps going if there is an error in the loop
 
 ###################################################################################################
 ###################################################################################################
 ##Load Packages
 ###################################################################################################
 ###################################################################################################
-rm(list=ls(all=TRUE))
 library(rvest)
 library(stringr)
 
@@ -22,99 +22,7 @@ library(stringr)
 url <- paste0("https://www.glassdoor.com/Job/data-scientist-jobs-SRCH_KO0,14_IP",1,".htm")
 urls <- paste0("https://www.glassdoor.com/Job/data-scientist-jobs-SRCH_KO0,14_IP", 1:33, ".htm")
 
-
-###################################################################################################
-##  creates fields which are the individual job posts 
-###################################################################################################
-
-download.file(url, destfile = "scrapedpage.html", quiet=TRUE)
-fields <-read_html("scrapedpage.html") %>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "jl", " " ))]')
-
-###################################################################################################
-##  creates job title which are the individual job posts 
-###################################################################################################
-title <- fields %>% html_nodes(".flexbox .jobLink") %>% html_text() %>% trimws()
-
-###################################################################################################
-## gets salaries and replaces not listed when salary information is missing
-###################################################################################################
-salaries <- sapply(fields, function(x) {
-  tmp <- html_nodes(x, ".small") %>% html_text()
-  ifelse(length(tmp) == 0, "Not listed", trimws(tmp))
-})
-salaries <- trimws(gsub("(Glassdoor est.)", "", salaries))
-
-####################################################################################################
-##  gets job location 
-###################################################################################################
-
-locations <- fields %>% html_nodes(".loc") %>% html_text() %>% trimws()
-
-
-####################################################################################################
-##  gets emplyer, employer id and job id 
-###################################################################################################
-
-##can;t figure out how to get gsub or strreplace to recognize the - to remove the city name
-employer <- fields %>% html_nodes(".empLoc") %>% html_text() %>% trimws()
-
-employer_id<-fields %>% html_attr("data-emp-id")
-
-job_id<-fields %>% html_attr("data-id")
-
-####################################################################################################
-##  creates job urls from job id, note if this part breaks, just replace the first part of the past 0 with another link
-###################################################################################################
-job.urls <- paste0("https://www.glassdoor.com/job-listing/data-scientist-emc-research-JV_IC1145845_KO0,14_KE15,27.htm?jl=",
-                   job_id)
-
-####################################################################################################
-## gets the rae job description, days posted, and employed from the job posts
-###################################################################################################
-
-more_job_facts <- sapply(job.urls, function(x) {
-  Sys.sleep(5)
-  download.file(x, destfile = "scrapedpage.html", quiet=TRUE)
-  website<- read_html("scrapedpage.html")
-  job_desc_raw <- website %>% html_nodes("#JobDescContainer") %>% html_text()
-  employer2 <- if ((website  %>% html_nodes(".padRtSm") %>% html_text())==""){
-    
-    "no employer listed"
-  } else{
-    
-    website  %>% html_nodes(".padRtSm") %>% html_text()
-  }
-    
-  c(job_desc_raw, employer2)
-})
-
-more_job_facts <- unname(more_job_facts)
-
-job_desc_raw<-more_job_facts[1,]
-employer2<-more_job_facts[2,]
-
-
-####################################################################################################
-##  creates employer urls from job id, note if this part breaks, just replace the first part of the past 0 with another link
-###################################################################################################
-
-emp.urls <- paste0("https://www.glassdoor.com/Overview/Working-at-ID-Analytics-EI_IE",
-                   employer_id,".11,23.htm")
-
-more_job_facts2 <- sapply(emp.urls, function(x) {
-  Sys.sleep(5)
-  download.file(x, destfile = "scrapedpage.html", quiet=TRUE)
-  
-  website<- read_html("scrapedpage.html")
-  
-  
-  emp_desc_raw <- website %>% html_nodes("#EmpBasicInfo") %>% html_text()
-
-})
-more_job_facts2 <- unname(more_job_facts2)
-
-df<-cbind(job_id, employer_id, title, salaries, locations, job_desc_raw, more_job_facts2, employer, employer2)
-
+df1<-data.frame()
 
 ####################################################################################################
 ####################################################################################################
@@ -174,6 +82,8 @@ for (i in 1:33){
   df1<-rbind(df1,df1a)
 }
  
+save(df1, file="glassdoor_df1.r")
+
 ####################################################################################################
 ####################################################################################################
 ####################################################################################################
@@ -198,11 +108,11 @@ for (i in 1:33){
   df1$employer2<-""
 #used to compare null results in loop  
   a <- character(0)
-
-  for (i in 493:1050)
+  n<-nrow(df1)
+  for (i in 1:n)
     {
 
-    Sys.sleep(2)
+    Sys.sleep(1)
     
       download.file(df1[i,]$job.urls, destfile = "scrapedpage.html", quiet=TRUE)
         
@@ -234,6 +144,8 @@ for (i in 1:33){
            )
   }
   
+  save(df1, file="glassdoor_df1.r")
+  
 ####################################################################################################
 ####################################################################################################
 ####################################################################################################
@@ -246,16 +158,16 @@ for (i in 1:33){
 ##  creates employer urls from job id, note if this part breaks, just replace the first part of the past 0 with another link
 ###################################################################################################
   
-  df1$emp.urls <- paste0("https://www.glassdoor.com/Overview/Working-at-ID-Analytics-EI_IE",
-                     df1$employer_id,".11,23.htm")
+  unique_emp_ids<-unlist(unique(df1$employer_id[df1$employer_id!=0]))
   
-
+  emp.urls <- paste0("https://www.glassdoor.com/Overview/Working-at-ID-Analytics-EI_IE",
+                     unique_emp_ids,".11,23.htm")
+  
+  df2<-data.frame(unique_emp_ids, emp.urls)
+  
 ####################################################################################################
 ##  gets empoyer description text
 ###################################################################################################
-  
-  unique_emp_urls<-unlist(unique(df1$emp.urls[df1$employer_id!=0]))
-  df2<-data.frame(unique_emp_urls)
 
   n<-nrow(df2)
 
@@ -263,12 +175,12 @@ for (i in 1:33){
 
  # df2<-df2[-c(387),]  
       
-  for (i in 540:n)
+  for (i in 1:n)
   {
     
-   Sys.sleep(2)
+  Sys.sleep(1)
     
-    download.file(as.character(df2$unique_emp_urls[i]), destfile = "scrapedpage.html", quiet=TRUE)
+    download.file(as.character(df2$emp.urls[i]), destfile = "scrapedpage.html", quiet=TRUE)
     
     website<- read_html("scrapedpage.html")
     
@@ -286,36 +198,21 @@ for (i in 1:33){
     
   }
   
-  length(unique(df1$emp.urls))
+  save(df2, file="glassdoor_df2.r")
   
-  to_ch
- 
-  df2[1,]$unique_emp_urls
+####################################################################################################
+####################################################################################################
+####################################################################################################
+####################################################################################################
+##  merge job and employer datasets
+####################################################################################################
+#################################################################################################### 
+#################################################################################################### 
+####################################################################################################
+
+colnames(df2)[1] <- "employer_id"
   
-  download.file(as.character(df2$unique_emp_urls[1]), destfile = "scrapedpage.html", quiet=TRUE)
-  
-  download.file(df1[i,]$job.urls, destfile = "scrapedpage.html", quiet=TRUE)
-  
-  df2$unique_emp_urls[387]
-  as.character(df2$unique_emp_urls[1])
-  df1[2,]$job.urls
-  
-  download.file(as.character(df2$unique_emp_urls[i]), destfile = "scrapedpage.html", quiet=TRUE)
-  
-  website<- read_html("scrapedpage.html")
-  
-  download.file(as.character(df2$unique_emp_urls[1]), destfile = "scrapedpage.html", quiet=TRUE)
-  
-  website<- read_html("scrapedpage.html")
-  
-  df2$emp_desc_raw[1] <- 
-    (if (identical(a,(website %>% html_nodes("#EmpBasicInfo") %>% html_text())))
-    {
-      "NO DESCRIPTION LISTED"
-    } 
-    
-    else
-    {
-      website %>% html_nodes("#EmpBasicInfo") %>% html_text()
-    }
-    )
+glassdoor_df<-merge(x = df1, y = df2, by = "employer_id", all.x = TRUE)
+
+save(glassdoor_df, file="glassdoor_df.r")
+
